20b4ddf43bd26ce6679faad48eb584cdcfc765cd
Kevin Liu <you@example.com>
2023-06-15 19:02:45 -0400

    Added Styles

--- src/IncrementDecrement/incrementDecrement.jsx
+++ src/IncrementDecrement/incrementDecrement.jsx
@@ -21,11 +21,36 @@ class IncrementDecrement extends Component {
     }
 
     render() {
+        const containerStyle = {
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center'
+        };
+      
+        const buttonStyle = {
+            margin: '0 10px',
+            padding: '10px 20px',
+            fontSize: '16px',
+            backgroundColor: '#337ab7',
+            color: '#fff',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer'
+        };
+      
+        const countStyle = {
+            fontSize: '24px',
+            fontWeight: 'bold',
+            margin: '20px 0'
+        };
+      
         return (
-            <div className="container">
-                <button onClick={this.increment}>Increment</button>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.decrement}>Decrement</button> 
+            <div style={containerStyle}>
+              <button style={buttonStyle} onClick={this.increment}>Increment</button>
+              <h1 style={countStyle}>{this.state.count}</h1>
+              <button style={buttonStyle} onClick={this.decrement}>Decrement</button>
             </div>
         );
     }
